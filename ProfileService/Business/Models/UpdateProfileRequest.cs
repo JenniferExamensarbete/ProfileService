67331c67077ab4c015c992179d28f3606cc85f28
Jecinaha <hagg.jennifer@gmail.com>
2026-05-18 10:57:35 +0200

    adding regular expression

--- ProfileService/Business/Models/UpdateProfileRequest.cs
+++ ProfileService/Business/Models/UpdateProfileRequest.cs
@@ -1,9 +1,27 @@
-﻿namespace ProfileService.Business.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfileService.Business.Models;
 
 public class UpdateProfileRequest
 {
+    [RegularExpression(
+        @"^[A-Za-zÅÄÖåäö\s-]{2,50}$",
+        ErrorMessage = "Förnamn får bara innehålla bokstäver och måste vara 2–50 tecken."
+    )]
     public string? FirstName { get; set; }
+
+    [RegularExpression(
+        @"^[A-Za-zÅÄÖåäö\s-]{2,50}$",
+        ErrorMessage = "Efternamn får bara innehålla bokstäver och måste vara 2–50 tecken."
+    )]
     public string? LastName { get; set; }
+
+    [RegularExpression(
+        @"^[0-9+\-\s]{6,20}$",
+        ErrorMessage = "Telefonnummer får bara innehålla siffror, mellanslag, + och -."
+    )]
     public string? Phone { get; set; }
+
+    [Url(ErrorMessage = "Bild måste vara en giltig URL.")]
     public string? ImageUrl { get; set; }
 }
